c29cab485cda39007b41ca74a1bc377d2eb08065
ayan-hassan <ahassan@hotmail.ca>
2023-03-06 19:27:15 -0500

    created queries using group by and having

--- 2_queries_joins/1_total_assignment_duration.sql
+++ 2_queries_joins/1_total_assignment_duration.sql
@@ -1,3 +1,12 @@
 SELECT SUM(assignment_submissions.duration) as total_duration
 FROM students JOIN assignment_submissions ON students.id = student_id
 WHERE students.name = 'Ibrahim Schimmel';
+
+
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING total_submissions < 100;
