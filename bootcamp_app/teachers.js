516ea26cb6f9b45dc94ca3d9b1128b797ccfc1bc
ayan-hassan <ahassan@hotmail.ca>
2023-03-08 19:48:36 -0500

    connected db to js with node-postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,35 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//     res.rows.forEach(user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//     });
+//   }).catch(err => console.error('query error', err.stack));
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohorts
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teachers.name;
+`)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: row.teachers`);
+    });
+  });
